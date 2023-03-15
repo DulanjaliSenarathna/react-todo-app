a68bfa0e1eecfb56d54a3f903be5baf972625755
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-15 15:38:55 +0530

    AppHeader component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PageTitle from './components/PageTitle';
+import AppHeader from './components/AppHeader';
+import style from './styles/modules/app.module.scss'
 
 function App() {
   return (
     <div className="container">
       <PageTitle>TODO LIST</PageTitle>
+      <div className={style.app__wrapper}>
+        <AppHeader />
+      </div>
     </div>
   );
 }
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// eslint-disable-next-line arrow-body-style
+const AppHeader = () => {
+  return (
+    <div>
+        <h1>hello from header</h1>
+    </div>
+  )
+}
+
+export default AppHeader
