45ff16b230489ee41fd7dabb39ee0c340dcf6208
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-15 23:18:32 +0530

    To Do Modal done

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button, { SelectButton } from './Button';
 import styles from '../styles/modules/app.module.scss'
+import ToDoModal from './ToDoModal';
 
 // eslint-disable-next-line arrow-body-style
 const AppHeader = () => {
+
+  const [modalOpen, setModalOpen] = useState(true);
   return (
     <div className={styles.appHeader}>
         
-        <Button varient='primary'>Add Task</Button>
+        <Button varient='primary' onClick={()=> setModalOpen(true)}>Add Task</Button>
         <SelectButton id="status">
             <option value="all">ALL</option>
             <option value="incomplete">Incomplete</option>
             <option value="complete">Complete</option>
         </SelectButton>
+        <ToDoModal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
     </div>
   )
 }
