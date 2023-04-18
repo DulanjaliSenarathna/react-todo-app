ca567a6f76ba52dd572476b2e656acc862f296d5
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-18 16:58:18 +0530

    UI updated

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import ToDoItem from './ToDoItem';
+import styles from '../styles/modules/app.module.scss'
 
 function AppContent() {
   // get todos from localstorage
@@ -9,7 +10,7 @@ function AppContent() {
   sortedTodoList.sort((a,b) => new Date(b.time) - new Date(a.time));
   console.log(todoList);
   return (
-    <div>
+    <div className={styles.content__wrapper}>
       {sortedTodoList && sortedTodoList.length > 0 ? 
        sortedTodoList.map((todo)=><ToDoItem key={todo.id} todo={todo}/>)
       : 'no to do found'}
