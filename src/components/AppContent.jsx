dd314ada0a1c09e64de5e2e2ab8394240709ae8d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-18 22:00:02 +0530

    Filter todos accordingly status done

--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -6,13 +6,24 @@ import styles from '../styles/modules/app.module.scss'
 function AppContent() {
   // get todos from localstorage
   const todoList = useSelector(state => state.todo.todoList);
+
+  // get filter status from redux store
+  const filterStatus = useSelector((state)=>state.todo.filterStatus);
   const sortedTodoList = [...todoList];
   sortedTodoList.sort((a,b) => new Date(b.time) - new Date(a.time));
+
+  const filteredTodoList = sortedTodoList.filter(item =>{
+    if(filterStatus === 'all'){
+      return true;
+    }
+    return item.status === filterStatus;
+  })
+
   console.log(todoList);
   return (
     <div className={styles.content__wrapper}>
-      {sortedTodoList && sortedTodoList.length > 0 ? 
-       sortedTodoList.map((todo)=><ToDoItem key={todo.id} todo={todo}/>)
+      {filteredTodoList && filteredTodoList.length > 0 ? 
+       filteredTodoList.map((todo)=><ToDoItem key={todo.id} todo={todo}/>)
       : 'no to do found'}
     </div>
   )
