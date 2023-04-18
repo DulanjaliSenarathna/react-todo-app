547f9d0bb6502643eca59530507e279b65de644a
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-18 16:55:06 +0530

    Check box functionality completed

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import {MdDelete, MdEdit} from 'react-icons/md';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-hot-toast';
-import { deleteTodo } from '../slices/todoSlice';
+import { deleteTodo, updateTodo } from '../slices/todoSlice';
 import styles from '../styles/modules/todoItem.module.scss'
 import { getClasses } from '../utils/getClasses';
 import ToDoModal from './ToDoModal';
-
-
-
+import CheckButton from './CheckButton';
 
 function ToDoItem({todo}) {
 
 // get the dispatch
 const dispatch = useDispatch();
 
+const [checked, setChecked] = useState(false);
+
 // create state for modalopen
 const [updateModalOpen, setUpdateModalOpen] = useState(false);
+
+useEffect(()=>{
+  if(todo.status === 'complete'){
+    setChecked(true);
+  }else{
+    setChecked(false);
+  }
+},[todo.status])
 
   const handleDelete = () =>{
     dispatch(deleteTodo(todo.id));
     toast.success('ToDo Deleted Successfully')
-  }
+  };
 
   const handleUpdate = () =>{
     setUpdateModalOpen(true);
+  };
+
+  const handleCheck = () =>{
+    setChecked(!checked);
+    dispatch(updateTodo({
+      ...todo,
+      status:checked ? 'incomplete' : 'complete'
+    }));
   }
 
   return (
     <>
      <div className={styles.item}>
         <div className={styles.todoDetails}>
-            []
+            <CheckButton checked={checked} handleCheck={handleCheck}/>
             <div className={styles.texts}>
                 <p className={getClasses([styles.todoText, todo.status === 'complete' && styles['todoText--completed']])}>{todo.title}</p>
                 <p className={styles.time}>{format(new Date(todo.time), 'p, MM/dd/yyyy')}</p>
